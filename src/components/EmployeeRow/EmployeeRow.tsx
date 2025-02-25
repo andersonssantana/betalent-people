68c5efd6a50d3adb1b19c803cebb9fd73c3c6153
Anderson Santana <anderslash@gmail.com>
2025-02-24 22:14:20 -0300

    ✨ Enhance EmployeeRow component with chevron icon and updated styling

--- src/components/EmployeeRow/EmployeeRow.tsx
+++ src/components/EmployeeRow/EmployeeRow.tsx
@@ -1,5 +1,6 @@
 import { formatDate, formatPhone } from "../../utils/formatters";
 import { Employee } from "../../types";
+import { FaChevronDown } from 'react-icons/fa';
 import './EmployeeRow.css';
 
 function EmployeeRow ({ employee }: { employee: Employee }) {
@@ -8,7 +9,12 @@ function EmployeeRow ({ employee }: { employee: Employee }) {
       <td className="table__td">
         <img src={employee.image} alt={employee.name} className="profile-image" />
       </td>
-      <td className="table__td">{employee.name}</td>
+      <td className="table__td">
+        <div className="name-with-icon">
+          {employee.name}
+          <FaChevronDown className="chevron-icon" />
+        </div>
+      </td>
       <td className="table__td">{employee.job}</td>
       <td className="table__td">{formatDate(employee.admission_date)}</td>
       <td className="table__td">{formatPhone(employee.phone)}</td>
